23d955cd3675a59157d91ea267107af957de6664
shivdip <sadarshivdip@gmail.com>
2026-02-23 12:07:16 +0530

    11

---
+++ 10/script.js
@@ -0,0 +1,94 @@
+// wap program to check number is even or odd
+
+// let a = 0;
+
+
+// if(a%2==0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// }
+
+
+// check falsy values
+
+// if (a) {
+//     console.log(`a = ${a}  is truthy`)
+    
+// } else {
+//     console.log(`a = ${a} is falsy`)
+// }
+
+
+// wap to print gretest value among three values
+
+
+// let a = 2;
+// let b = 4;
+// let c = 10;
+
+// let day = 2
+// switch (day) {
+//     case "2":
+//         console.log( "case 1 run ")
+//        break;
+
+//     case "3":
+//         console.log( "case 2 run ")
+//        break;
+
+//     default:
+//         console.log("not a valid day")
+//         break;
+// }
+
+// wap to select language using swich case
+
+
+
+//loop
+
+// let a = [2,"dev", 232.32, true,{1:"bhushan", 2:"om"}]  --valid array
+
+
+let b =[2,5,2,8,6,46,5];
+let target = 5;
+
+// for(let i=0;i<b.length;i++){
+//     if(b[i] == target){
+//         console.log(`target present at index ${i}`)
+//        break;
+//     }
+// }
+
+
+// same program uing do and do while loop
+
+// while (condition) {
+    
+// }
+
+// do {
+    
+// } while (condition);
+
+
+b.forEach(e => {
+    console.log(e);
+});
+
+// for each syntax 
+
+// array_name.forEach(index =>  {body} );
+
+
+// b.push(2);
+
+
+
+// console.log(b);
+
+
+
+
+// console.log(a);
---
+++ 11/script.js
@@ -0,0 +1,92 @@
+// let a =2;
+
+// console.log(a++);
+
+
+//  console.log(++a);
+
+
+// functions 
+
+
+
+// function sum(){
+//     console.log("hello")
+// }
+
+// sum();
+
+
+
+
+
+// wap using function to print full name by using first name and last name as arguments 
+
+// function fullname(first_name , last_name){
+//     console.log(first_name + " " + last_name);
+// }
+
+// fullname("shivdip","sadar")
+
+
+// const  greet =  function (){
+//     console.log("hello")
+// }
+
+// greet();
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+
+// const sum  = (a,b,c=4)=>{
+//         return a+b+c;
+
+// }
+
+// let addition = sum(3,5,2);
+// console.log(addition)
+
+
+
+
+//wap using arrow function
+
+
+
+// {
+//     let  a = 2;
+// }
+
+// let a = 4
+
+
+// console.log(a);
+
+
+
+// console.log( false || 0  || 3 )
+
+
+// let a = [1,3,4, function sum(){return a+b}]
+let a = "hello"
+
+// let s = a.replace("h","H");
+
+
+
+// console.log(s.at(2))
+// console.log(s)
+
+// console.log(a.slice(-3))
+
+// let s = "welcome to devlunchpad"
+
+// // print "come to dev" using slice 
+// console.log(s.split(" ",2));
+
+
+
+
+// wap using array to check prime numbers in it ;
+// return array of prime numbers in string format -- ["3", "5"]
