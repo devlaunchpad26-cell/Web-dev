09b260a637f037cc6fb757287daf4dcbe86e9586
shivdipsadar <sadarshivdip@gmail.com>
2026-03-27 10:37:37 +0530

    20

--- 20/backend/index.js
+++ 20/backend/index.js
@@ -1,8 +1,27 @@
 import express from "express"
 import cors from "cors"
 import todorouter from "./routes/todo.routes.js";
+import env from "dotenv";
+import mongoose from "mongoose";
+
+env.config();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT 
+const MONGODB_URI = process.env.MONGO_URI
+
+async function connectdb(){
+    try {
+        const connobj = await mongoose.connect(MONGODB_URI);
+        console.log("connection success");
+    
+    } catch (error) {
+        console.log("mongodb error", error);
+    }
+}
+
+connectdb();
+
+
 app.use(cors());
 app.use(express.json());
 app.use("/api",todorouter)
